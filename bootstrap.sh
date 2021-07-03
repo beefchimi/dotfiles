2b3af5e33f42f1500b0e9af5a888b579a3a82238
beefchimi <curtdulmage@gmail.com>
2021-07-03 11:51:42 -0400

    :hammer: [Spin] Add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,37 @@
+#!/bin/zsh
+
+###
+### Shopify Spin bootstrap script
+
+# Manually clone private dotfiles repo
+# git clone -b shopify --single-branch git@github.com:beefchimi/dotfiles.git
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install ZSH plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Install Starship prompt
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+
+# Shell config
+ln -sfn ~/dotfiles/.config-shell ~/.config-shell
+
+# Starship config
+ln -sfn ~/dotfiles/.config ~/.config
+
+# ZSH
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+
+# Git
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
+
+# GPG
+gpgconf --launch dirmngr
+gpg --keyserver keys.openpgp.org --recv D5CBA8EA14BEBE3FF3E0D21945A002F7F81F19B9
+
+source ~/.zshrc
