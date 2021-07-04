1c96a4ccbcb8a38b05365418795dc2df55b3f763
beefchimi <curtdulmage@gmail.com>
2021-07-04 16:22:46 -0400

    :sparkles: [ZSH] New alias for spin-post-install

--- .config-shell/zsh/aliases.zsh
+++ .config-shell/zsh/aliases.zsh
@@ -7,3 +7,4 @@ alias ohmyzsh="code ~/.oh-my-zsh"
 # Shopify
 alias brewery="brew update && brew upgrade && brew cleanup"
 alias devosw="dev cd shopify && git pull && dev cd web && git pull && dev cd online-store-web"
+alias spi="source ~/spin-post-install.sh"
--- spin-post-install.sh
+++ spin-post-install.sh
@@ -3,16 +3,20 @@
 ###
 ### Shopify Spin post-install script
 
-# Install Starship prompt (force "yes")
-if ! command -v starship &> /dev/null; then
-  sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --force
-fi
-
-# Install exa using Nix... as `apt install` does not seem to work
-if ! command -v exa &> /dev/null; then
-  if command -v nix-env &> /dev/null; then
-    nix-env -i exa
+if [[ $SPIN ]]; then
+  # Install Starship prompt (force "yes")
+  if ! command -v starship &> /dev/null; then
+    sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --force
   fi
+
+  # Install exa using Nix... as `apt install` does not seem to work
+  if ! command -v exa &> /dev/null; then
+    if command -v nix-env &> /dev/null; then
+      nix-env -i exa
+    fi
+  fi
+else
+  echo "Not in Spin... skipping command installations!"
 fi
 
 # Reload ZSH
