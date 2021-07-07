1c0706a8f1d581e27269fb6bfe0bc19d2ced7011
beefchimi <curtdulmage@gmail.com>
2021-07-07 10:16:37 -0400

    :bug: [Spin] Only set exa aliases if command exists

--- .config-shell/aliases.sh
+++ .config-shell/aliases.sh
@@ -5,7 +5,9 @@ alias dotf="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 alias gitcleanpull="git gc && git gc --auto && git prune && git fetch -p && git gc --prune=now && git pull"
 
 # Exa color lists
-alias ls="exa --icons"
-alias ll="exa -lbGF --icons --git"
-alias la="exa -lbhHigUmuSa@ --time-style=long-iso --icons --git --color-scale"
-alias lt="exa --icons --tree --level=2"
+if command -v exa &> /dev/null; then
+  alias ls="exa --icons"
+  alias ll="exa -lbGF --icons --git"
+  alias la="exa -lbhHigUmuSa@ --time-style=long-iso --icons --git --color-scale"
+  alias lt="exa --icons --tree --level=2"
+fi
