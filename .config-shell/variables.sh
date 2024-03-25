c95ea8724f497707b3aa84722816ea55b81818d0
beefchimi <curtdulmage@gmail.com>
2024-03-25 10:33:05 -0400

    :sparkles: [Variables] Export more paths for Android development

--- .config-shell/variables.sh
+++ .config-shell/variables.sh
@@ -14,3 +14,8 @@ export PROMPT_DITHER_END="░▒▓█╣"
 
 # Set the JAVA_HOME environment variable for Android developlment
 export JAVA_HOME="/Applications/Android Studio.app/Contents/jbr/Contents/Home"
+
+# Export the Android SDK variables.
+# The NDK path may need to be regularly updated.
+export ANDROID_HOME="$HOME/Library/Android/sdk"
+export NDK_HOME="$ANDROID_HOME/ndk/26.2.11394342"
